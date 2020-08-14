97d8633b9bee934c1849e20b64cb8a8301c9c5e5
Kashish Jain <jainabhishek7204@gmail.com>
2020-08-14 16:51:16 +0530

    ..

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import HomeBanner from './components/HomeBanner';
 import Categories from './components/Categories';
+import { SetTitle } from './components/SetTitle';
 import './css/untitled.css';
 
 const Home = () => {
     return (
         <div>
-            {/* <Nav /> */}
+            <SetTitle title="Home - Mummy Da Dhaba" />
             <HomeBanner />
             <Categories />
         </div>
