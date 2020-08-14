e8dd0d9a8d169f6f30c23ddceac7f25640e9ae0a
Kashish Jain <jainabhishek7204@gmail.com>
2020-08-14 21:09:07 +0530

    Lazy() Loading Style

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { lazy, Suspense } from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-// import Home from './Home';
 const Home = lazy(() => import('./Home'));
 const Category = lazy(() => import('./components/Category'));
 const Dish = lazy(() => import('./Dish'));
-// import Category from './components/Category';
-// import Dish from './Dish';
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={
+        <div style={{marginTop: "50vh", marginBottom: "50vh", marginLeft: "50vw"}} class="spinner-border" role="status">
+          <span class="mx-auto sr-only">Loading...</span>
+        </div>
+      }>
         <Nav />
         <Route path="/category/:name" component={Category} />
         <Route path="/dish/:id" component={Dish} />
